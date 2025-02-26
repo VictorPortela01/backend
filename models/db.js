25861ca0bddd6bf34a21e223f509d4b61e3dc9e1
Victor <victorportela1010@gmail.com>
2025-02-26 11:59:16 -0300

    Corrigindo erro de database

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('metas','root', '', {
-    host:"localhost",
+const sequelize = new Sequelize('metas','u312179767_meu_usuario', 'Dis@1004719', {
+    host:"https://auth-db1894.hstgr.io/index.php?db=u312179767_meu_banco",
     dialect:"mysql"
 })
 
